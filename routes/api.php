aecb9bfae1bab081b2b010737622f4ca154a7553
Jaydee-31 <johndextergamo@gmail.com>
2024-03-17 22:04:02 +0800

    api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'users'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    // Route::post('add', [UserController::class, 'add']);
+    // Route::post('update', [UserController::class, 'update']);
+
+    // Route::post('/create_product', [UserController::class, 'createProduct']);
+});
