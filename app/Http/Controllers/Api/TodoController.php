fd32947cc93aabb4eb51d09a416b7c6d50e8c188
Jaydee-31 <johndextergamo@gmail.com>
2024-03-20 20:56:35 +0800

    initialized routes & api

---
+++ app/Http/Controllers/Api/TodoController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TodoRequest;
+use App\Http\Resources\TodoResource;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return TodoResource::collection(Todo::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(TodoRequest $request)
+    {
+        $todo = Todo::create($request->validated());
+        return new TodoResource($todo);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Todo $todo)
+    {
+        return new TodoResource($todo);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(TodoRequest $request, Todo $todo)
+    {
+        $todo->update($request->validated());
+        return new TodoResource($todo);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+        return response()->noContent();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -8,10 +9,17 @@ use App\Http\Controllers\UserController;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
+// Route::apiResource('todos', TodoController::class);
+Route::group(['prefix' => 'todo'], function () {
+    Route::get('list', [TodoController::class, 'index']);
+    Route::post('create', [TodoController::class, 'store']);
+    Route::get('view/{todo}', [TodoController::class, 'show']);
+    Route::match(['put', 'patch'], 'update/{todo}', [TodoController::class, 'update']);
+    Route::delete('delete/{todo}', [TodoController::class, 'destroy']);
+});
+
 Route::group(['prefix' => 'users'], function () {
     Route::get('/', [UserController::class, 'index']);
     // Route::post('add', [UserController::class, 'add']);
     // Route::post('update', [UserController::class, 'update']);
-
-    // Route::post('/create_product', [UserController::class, 'createProduct']);
 });
