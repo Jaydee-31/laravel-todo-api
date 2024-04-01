cf1c4034e402b1ba7478e2a213497545977038eb
Jaydee-31 <johndextergamo@gmail.com>
2024-04-01 14:21:21 +0800

    Added user resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'profile_photo_path' => $this->profile_photo_path,
+            'image_url' => $this->profile_photo_path ? URL::to($this->profile_photo_path) : null,
+        ];
+    }
+}
