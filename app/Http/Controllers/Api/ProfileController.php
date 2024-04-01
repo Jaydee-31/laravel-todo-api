76e28925f678d83d2fbccf760830db06cd744e2c
Jaydee-31 <johndextergamo@gmail.com>
2024-04-01 12:44:53 +0800

    user profile

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use App\Http\Requests\UpdateProfileRequest;
-use App\Models\User;
 
 class ProfileController extends Controller
 {
@@ -26,15 +28,27 @@ class ProfileController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProfileRequest $request, string $id)
+    public function update(UpdateProfileRequest $request, User $user)
     {
-        $request->user()->fill($request->validated());
+        $data = $request->user()->fill($request->validated());
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
+        // Check if image was given and save on local file system
+        if (isset($data['profile_photo_path'])) {
+            $relativePath = $this->saveImage($data['profile_photo_path']);
+            $data['profile_photo_path'] = $relativePath;
+
+            // If there is an old profile_photo_path, delete it
+            if ($user->profile_photo_path) {
+                $absolutePath = public_path($user->profile_photo_path);
+                File::delete($absolutePath);
+            }
         }
 
-        $request->user()->save();
+        if ($data->isDirty('email')) {
+            $data->email_verified_at = null;
+        }
+
+        $data->save();
 
         return response()->json([
             'success' => true
@@ -58,5 +72,47 @@ class ProfileController extends Controller
         return response()->json([
             'success' => true
         ]);
+    }
+
+    /**
+     * Save image in local file system and return saved image path
+     *
+     * @param $image
+     * @throws \Exception
+     * @author Zura Sekhniashvili <zurasekhniashvili@gmail.com>
+     */
+    private function saveImage($image)
+    {
+        // Check if image is valid base64 string
+        if (preg_match('/^data:image\/(\w+);base64,/', $image, $type)) {
+            // Take out the base64 encoded text without mime type
+            $image = substr($image, strpos($image, ',') + 1);
+            // Get file extension
+            $type = strtolower($type[1]); // jpg, png, gif
+
+            // Check if file is an image
+            if (!in_array($type, ['jpg', 'jpeg', 'gif', 'png'])) {
+                throw new \Exception('invalid image type');
+            }
+            $image = str_replace(' ', '+', $image);
+            $image = base64_decode($image);
+
+            if ($image === false) {
+                throw new \Exception('base64_decode failed');
+            }
+        } else {
+            throw new \Exception('did not match data URI with image data');
+        }
+
+        $dir = 'storage/images/';
+        $file = Str::random() . '.' . $type;
+        $absolutePath = public_path($dir);
+        $relativePath = $dir . $file;
+        if (!File::exists($absolutePath)) {
+            File::makeDirectory($absolutePath, 0755, true);
+        }
+        file_put_contents($relativePath, $image);
+
+        return $relativePath;
     }
 }
